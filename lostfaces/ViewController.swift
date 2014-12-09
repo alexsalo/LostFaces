9160e045e591a31fe585dc06d1107e568a3669ba
Aleksandr Salo <salo@gamemac01.ecs.baylor.edu>
2014-12-09 13:32:14 -0600

    Initial Commit

---
+++ lostfaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lostfaces
+//
+//  Created by Aleksandr Salo on 12/9/14.
+//  Copyright (c) 2014 Aleksandr Salo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lostfacesTests/lostfacesTests.swift
@@ -0,0 +1,36 @@
+//
+//  lostfacesTests.swift
+//  lostfacesTests
+//
+//  Created by Aleksandr Salo on 12/9/14.
+//  Copyright (c) 2014 Aleksandr Salo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lostfacesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
